484f4b7e79850ea4d45144f3e813129619d535d5
Shriya <shriyachhabra98@gmail.com>
2018-02-15 23:01:00 +0530

    verify code

--- LogophileBot.py
+++ LogophileBot.py
@@ -59,6 +59,6 @@ def send_message(recipient_id, response):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 6000))
     app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
