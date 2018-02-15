47000bcf1478c98b40881d04b3d736f73eb99cd2
Shriya <shriyachhabra98@gmail.com>
2018-02-16 00:35:12 +0530

    wordExists

--- Modules/wordExists.py
+++ Modules/wordExists.py
@@ -9,13 +9,12 @@ def word_exists(word):
  word_id = word
  url = 'https://od-api.oxforddictionaries.com:443/api/v1/inflections/' + language + '/' + word_id.lower()
  r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
- code="code {}\n".format(r.status_code)
+ code=r.status_code
  print(code)
 
- if(code=='404'):
-     return "The word does not exist"
+ if(code==404):
+   return "The word does not exist"
 
- return """Great!The word exists.
-          You know the language well."""
+ return "Great!The word exists.\nYou know the language well."
 # print("text \n" + r.text)
 # print("json \n" + json.dumps(r.json()))
