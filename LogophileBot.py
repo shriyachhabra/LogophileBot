5ab42194c2459fdd91bdb3d591aa251dac46c093
Shriya <shriyachhabra98@gmail.com>
2018-02-18 17:02:03 +0530

    synonym

--- LogophileBot.py
+++ LogophileBot.py
@@ -2,6 +2,7 @@ from flask import Flask,request
 from pymessenger.bot import Bot
 from Modules.wordExists import *
 from Modules.meaning import *
+from Modules.synonyms import *
 import os
 app = Flask(__name__)
 
@@ -45,6 +46,8 @@ def handle_mssg(rec_id,msg):
         response_text=word_exists(txt[1])
     elif(txt[0]=="meaning?"):
         response_text=word_meaning(txt[1])
+    elif(txt[0]=="synonym?"):
+        response_text=synonyms(txt[1])
     send_message(rec_id,response_text)
 
     return "handled"
---
+++ Modules/synonyms.py
@@ -0,0 +1,32 @@
+import requests
+import json
+
+def synonyms(word):
+ app_id = 'd520dc72'
+ app_key = '9636a335f1d422e70aa5a9b2381f6517'
+
+ language = 'en'
+ word_id =word
+
+ url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word_id.lower() + '/synonyms'
+
+ r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
+
+ code=r.status_code
+ if(code==404):
+     return "Word does not exist"
+ r=r.json()
+ results=r['results'][0]
+ le=results['lexicalEntries'][0]
+ entries=le['entries'][0]
+ senses=entries['senses'][0]
+ subsenses=senses['subsenses'][0]
+ synonyms=subsenses['synonyms']
+ synDic=""
+ for item in synonyms:
+    id=item['id']
+    synDic+=id+"\n"
+
+
+ print(synDic)
+ return synDic
