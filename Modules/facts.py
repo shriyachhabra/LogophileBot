252e73782326601b30070c3b5b8dad14966d0921
Shriya <shriyachhabra98@gmail.com>
2018-02-20 23:48:52 +0530

    facts

--- Modules/facts.py
+++ Modules/facts.py
@@ -1,29 +1,8 @@
-# from bs4 import BeautifulSoup as BS
-# import requests
-# import random
-#
-# # def get_facts():
-# req="https://www.lingoda.com/en/blog/fun-facts-english-language"
-# res=requests.get(req)
-# soup = BS(res.text, 'html.parser')
-# list=[x.text for x in soup.find_all('ol')]
-#     # return random.choice(list)
-#
-# p=random.choice(list)
-# print(p)
-import requests
-from bs4 import BeautifulSoup as BS
 import random
+from Modules.facts66 import *
 
 def get_facts():
-    req = 'https://www.bloomsbury-international.com/blog/2014/07/11/15-interesting-facts-about-the-english-language/'
-    res = requests.get(req)
-    soup = BS(res.text, 'html.parser')
-    list = [x.text for x in soup.find_all('p')]
-    list=list[3:18]
-    # for item in list:
-    #     print(item+"\n")
-    fact=random.choice(list)
-    fact=fact.split(' ',1)
-    return fact[1]
-# print(get_quotes())
+  f=random.choice(fact)
+  return f
+
+print(get_facts())
---
+++ Modules/facts66.py
@@ -0,0 +1,132 @@
+fact=[
+" In the 17th century, magpies were nicknamed pie-maggots.",
+
+"The part of a wall between two windows is called the interfenestration.",
+
+"If you were to write out every number name in full (one, two, three, four...), you wouldn’t use a single letter B until you reached one billion.",
+
+"The part of your back that you can’t quite reach to scratch is called the acnestis. It’s derived from the Greek word for “cheese-grater.”",
+
+"A hecatompedon is a building measuring precisely 100ft × 100ft.",
+
+"A growlery is a place you like to retire to when you’re unwell or in a bad mood. It was coined by Charles Dickens in Bleak House (1853).",
+
+"There was no word for the color orange in English until about 450 years ago.",
+
+"The infinity sign, ∞, is called a lemniscate. Its name means “decorated with ribbons” in Latin.",
+
+"A Dutch feast is one at which the host gets drunk before his hosts do.",
+
+"Schoolmaster is an anagram of “the classroom.”",
+
+"To explode originally meant “to jeer a performer off the stage.”",
+
+"Funk was originally a Tudor word for the stale smell of tobacco smoke.",
+
+"In written English, only one letter in every 510 is a Q.",
+
+"The opposite of déjà-vu is called jamais-vu: it describes the odd feeling that something very familiar is actually completely new.",
+
+"A scissor was originally a type of Roman gladiator thought to have been armed either with a pair of swords or blades, or with a single dual-bladed dagger.",
+
+"To jirble means “to spill a liquid while pouring it because your hands are shaking.”",
+
+"Samuel Johnson defined a sock as “something put between the foot and the shoe.”",
+
+"In Victorian slang, muffin-wallopers were old unmarried or widowed women who would meet up to gossip over tea and cakes.",
+
+"Scarecrows were once known as hobidy-boobies.",
+
+"The longest English word with its letters in reverse alphabetical order is spoonfeed.",
+
+"Shakespeare used the word puking in As You Like It.",
+
+"Flabellation is the use of a fan to cool something down.",
+
+"Bamboozle derives from a French word, embabouiner, meaning “to make a baboon out of someone.”",
+
+"A percontation is a question that requires more than a straightforward “yes” or “no” answer.",
+
+"The shortest -ology is oology, the scientific study of eggs.",
+
+"As a verb rather than a noun, owl means “to act wisely, despite knowing nothing.”",
+
+"A shape with 99 sides would be called an enneacontakaienneagon.",
+
+"In the 18th century, a clank-napper was a thief who specialized in stealing silverware.",
+
+"Noon is derived from the Latin for “ninth,” novem. It originally referred to the ninth hour of the Roman day — 3pm.",
+
+"11% of the entire English language is just the letter E.",
+
+"Oysterhood means “reclusiveness,” or “an overwhelming desire to stay at home.”",
+
+"A puckfist is someone who braggingly dominates a conversation.",
+
+"The bowl formed by cupping your hands together is called a gowpen.",
+
+"To battologize means “to repeat a word so incessantly in conversation that it loses all meaning and impact.”",
+
+"A zoilist is an unfair or unnecessarily harsh critic, or someone who particularly enjoys finding fault in things.",
+
+"In 19th century English, a cover-slut was a long cloak or overcoat worn to hide a person’s untidy or dirty clothes underneath.",
+
+"Happy is used three times more often in English than sad.",
+
+"Trinkgeld is money meant only to be spent on drink.",
+
+"Aquabob is an old name for an icicle.",
+
+"In the 16th and 17th century, buttock-mail was the name of a tax once levied in Scotland on people who had sex out of wedlock.",
+
+"Witzelsucht is a rare neurological disorder whose sufferers have an excessive tendency to tell pointless stories or inappropriate jokes and puns.",
+
+"A repdigit is a number comprised of a series of repeated numbers, like 9,999.",
+
+"In Tudor English, a gandermooner was a man who flirted with other women while his wife recovered from childbirth.",
+
+"A cumberground is an utterly useless person who literally serves no other purpose than to take up space.",
+
+"Sermocination is the proper name for posing a question and then immediately answering it yourself.",
+
+"The earliest known reference to baseball in English comes from Jane Austen’s Northanger Abbey (1798).",
+
+"Whipper-tooties are pointless misgivings or groundless excuses for not trying to do something.",
+
+"Anything described as transpontine is located on the opposite side of a bridge.",
+
+"In the early days of Hollywood, the custard pies thrown in comedy sketches were nicknamed magoos.",
+
+"Checkbook is the longest horizontally symmetrical word in the English language — although if proper nouns are included, Florida’s Lake Okeechobee is one letter longer.",
+
+"The earliest record of the phrase “do-it-yourself” comes from a 1910 magazine article about students at Boston University being left to teach themselves.",
+
+"The paddywhack mentioned in the nursery rhyme “This Old Man” is a Victorian slang word for a severe beating.",
+
+"The Kelvin temperature scale, the forsythia plant, Boeing aircraft and the state of Pennsylvania are all named after people called William.",
+
+"Xenoglossy is the apparent ability to speak a language that you’ve never actually learned.",
+
+"Mochas are named after a port in Yemen, from where coffee was exported to Europe in the 18th century.",
+
+"In mediaeval Europe, a moment was precisely 1/40th of an hour, or 90 seconds.",
+
+"To quomodocunquize means “to make money by whatever means possible.”",
+
+"Porpoise literally means “pork-fish.”",
+
+"Shivviness is an old Yorkshire word for the uncomfortable feeling of wearing new underwear.",
+
+"The adjectival form of abracadabra is abracadabrant, used to describe anything that has apparently happened by magic.",
+
+"Straitjackets were originally called strait-waistcoats.",
+
+"Aspirin and heroin were both originally trademarks. They lost their trademark status as part of the Treaty of Versailles.",
+
+"An autological word is one that describes itself — like short or unhyphenated.",
+
+"In the 18th century, teachers were nicknamed “haberdashers of pronouns.”",
+
+"The burnt or used part of a candlewick is called the snaste.",
+
+"The expressions “bully pulpit” and “lunatic fringe” were coined by Theodore Roosevelt."]
